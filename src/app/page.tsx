90460bc265055c308640755f26a55eee513eb286
pgrobban <robert.sebescen@surikat.se>
2025-05-22 11:23:11 +0200

    use stack instead of grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { Box, Button, Typography, Grid } from "@mui/material";
+import { Box, Button, Typography, Grid, Stack } from "@mui/material";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -9,28 +9,28 @@ export default function Home() {
 	return (
 		<Box display="flex" flexDirection="column" alignItems="center">
 			<Image src="/images/cover.jpg" alt="Logo" width={700} height={563} />
-			<Grid mt={4} container spacing={4} direction="column" alignItems="center">
-				<Grid size={12}>
-					<Button fullWidth onClick={() => router.push("/create")}>
-						<Typography>Instructions</Typography>
-					</Button>
-				</Grid>
-				<Grid size={12}>
-					<Button fullWidth onClick={() => router.push("/join")}>
-						<Typography>Read the prologue text</Typography>
-					</Button>
-				</Grid>
-				<Grid size={12}>
-					<Button
-						sx={{
-							boxShadow: "0 0 10px rgba(173, 216, 230, 0.6)",
-							textShadow: "0 0 4px #fff",
-						}}
-					>
-						<Typography variant="button">ENTER THE MAZE</Typography>
-					</Button>
-				</Grid>
-			</Grid>
+			<Stack
+				mt={4}
+				spacing={4}
+				width={400}
+				direction="column"
+				alignItems="center"
+			>
+				<Button fullWidth onClick={() => router.push("/create")}>
+					<Typography>Instructions</Typography>
+				</Button>
+				<Button fullWidth onClick={() => router.push("/join")}>
+					<Typography>Read the prologue text</Typography>
+				</Button>
+				<Button
+					sx={{
+						boxShadow: "0 0 10px rgba(173, 216, 230, 0.6)",
+						textShadow: "0 0 4px #fff",
+					}}
+				>
+					<Typography variant="button">ENTER THE MAZE</Typography>
+				</Button>
+			</Stack>
 		</Box>
 	);
 }
