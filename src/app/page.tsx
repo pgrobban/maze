232c1efd4db814b7f0238af37f963f34d7c4944a
pgrobban <robert.sebescen@surikat.se>
2025-05-22 10:48:46 +0200

    added public files, changed theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,36 @@
+"use client";
 import Image from "next/image";
-import styles from "./page.module.css";
+import { Box, Button, Typography, Grid } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const router = useRouter();
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<Box display="flex" flexDirection="column" alignItems="center">
+			<Image src="/images/cover.jpg" alt="Logo" width={700} height={563} />
+			<Grid mt={4} container spacing={4} direction="column" alignItems="center">
+				<Grid size={12}>
+					<Button fullWidth onClick={() => router.push("/create")}>
+						<Typography>Instructions</Typography>
+					</Button>
+				</Grid>
+				<Grid size={12}>
+					<Button fullWidth onClick={() => router.push("/join")}>
+						<Typography>Read the prologue text</Typography>
+					</Button>
+				</Grid>
+				<Grid size={12}>
+					<Button
+						sx={{
+							boxShadow: "0 0 10px rgba(173, 216, 230, 0.6)",
+							textShadow: "0 0 4px #fff",
+						}}
+					>
+						<Typography variant="button">ENTER THE MAZE</Typography>
+					</Button>
+				</Grid>
+			</Grid>
+		</Box>
+	);
 }
---
+++ src/app/theme.ts
@@ -0,0 +1,47 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: '#90caf9',
+    },
+    background: {
+      default: '#121212',
+    },
+  },
+  typography: {
+    fontSize: 20,
+    fontFamily: "Garamond, serif",
+    button: {
+      fontWeight: 700,
+      letterSpacing: '0.05em',
+      fontSize: '1.1rem',
+      textTransform: 'none',
+    },
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: 'outlined',
+        fullWidth: true,
+      },
+      styleOverrides: {
+        root: {
+          borderWidth: 2,
+          borderColor: 'white',
+          color: 'white',
+          padding: '12px 16px',
+          fontFamily: '"Cinzel", serif',
+          '&:hover': {
+            backgroundColor: 'rgba(255, 255, 255, 0.1)',
+            borderColor: '#ccc',
+          },
+        },
+      },
+    }
+  }
+});
+
+export default theme;
